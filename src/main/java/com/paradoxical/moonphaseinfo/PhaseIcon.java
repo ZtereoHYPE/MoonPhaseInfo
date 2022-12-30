0cb1c2e5497ebc8852c29e6a134dfd5b3e53181d
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-12-30 17:09:15 +0100

    new: update to 1.19.3 and add transparency when F3 is open

--- src/main/java/com/paradoxical/moonphaseinfo/PhaseIcon.java
+++ src/main/java/com/paradoxical/moonphaseinfo/PhaseIcon.java
@@ -3,6 +3,7 @@ package com.paradoxical.moonphaseinfo;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
@@ -13,18 +14,22 @@ public class PhaseIcon extends DrawableHelper {
     private static final Formatting[] FULLNESS_COLOR = new Formatting[]
             {Formatting.RED, Formatting.GOLD, Formatting.YELLOW, Formatting.GREEN, Formatting.DARK_GREEN};
 
-    void drawPhaseIcon() {
+    void drawPhaseIcon(MatrixStack matrixStack) {
         MinecraftClient mc = MinecraftClient.getInstance();
         int windowWidth = mc.getWindow().getScaledWidth();
 
-        mc.getTextureManager().bindTexture(INVENTORY);
+        if (mc.options.debugEnabled) {
+            RenderSystem.setShaderColor(1f, 1f, 1f, 0.25f);
+        }
+
+        RenderSystem.setShaderTexture(0, INVENTORY);
         int a = 24;
         int x = windowWidth / 2 - a - 1;
         int y = 1;
-        blit(x, y, 141, 166, a, a);
+        drawTexture(matrixStack, x, y, 400, 141, 166, a, a, 256, 256);
 
         RenderSystem.enableBlend();
-        mc.getTextureManager().bindTexture(ICONS);
+        RenderSystem.setShaderTexture(0, ICONS);
 
         int a2 = 18;
         int d = a / 2 - a2 / 2;
@@ -34,22 +39,23 @@ public class PhaseIcon extends DrawableHelper {
         int phase = mc.world.getMoonPhase();
         int u = ((4 - phase % 4) % 4) * a2; //{0, 3, 2, 1, 0, 3, 2, 1}[phase] * side length
         int v = phase >= 1 && phase <= 4 ? a2 : 0;
-        blit(x, y, this.getBlitOffset(), (float)u, (float)v, a2, a2, 36, 72);
+        drawTexture(matrixStack, x, y, 400, (float)u, (float)v, a2, a2, 72, 36);
         RenderSystem.disableBlend();
 
         //fullness
+        int transparencyMask = mc.options.debugEnabled ? 0x50FFFFFF : 0xFFFFFFFF;
         int sizePercent = (int)(mc.world.getMoonSize() * 100f);
-        mc.textRenderer.drawWithShadow(String.format("%s%d%%%s",
+        mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
                 FULLNESS_COLOR[sizePercent / 25],
                 sizePercent,
                 phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
-                windowWidth / 2 + 1, 5, 0xffffff);
+                windowWidth / 2 + 1, 5, transparencyMask);
 
         //time left
         long daySecondsLeft = (24000 - mc.world.getTimeOfDay() % 24000) / 20;
         long dayMinutesLeft = daySecondsLeft / 60;
         boolean atLeast60s = daySecondsLeft >= 60;
-        mc.textRenderer.drawWithShadow(atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
-                windowWidth / 2 + 1, 5 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+        mc.textRenderer.drawWithShadow(matrixStack, Formatting.DARK_AQUA + (atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s"),
+                windowWidth / 2 + 1, 5 + mc.textRenderer.fontHeight, transparencyMask);
     }
 }
